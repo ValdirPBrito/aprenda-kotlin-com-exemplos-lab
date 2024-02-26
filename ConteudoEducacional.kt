5b6772778fed382cdefe27421af51299ec687a81
Valdir Brito <valdir_brito@hotmail.com>
2024-02-25 21:10:35 -0300

    Create ConteudoEducacional.kt

---
+++ ConteudoEducacional.kt
@@ -0,0 +1,7 @@
+data class ConteudoEducacional( val id: Int,
+                                val nome: String,
+                                val duracao: Int = 60,
+                                val descricao: String,
+                                val autor: String,
+                                val nivel: Nivel
+)
