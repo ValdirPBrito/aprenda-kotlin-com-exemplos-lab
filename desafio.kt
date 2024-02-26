470fd82c04539dcb71059f48e2c6cc3269a58f1d
Valdir Brito <valdir_brito@hotmail.com>
2024-02-25 21:06:27 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,27 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import entities.Formacao
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+    fun main(){
+        val listaUsuarios: MutableList<Usuario> = mutableListOf()
+        val listaConteudos: MutableList<ConteudoEducacional> = mutableListOf()
+        listaUsuarios.add(Usuario(1,"Antonio","antonio@gmail.com"))
+        listaUsuarios.add(Usuario(2,"Bill","bill@gmail.com"))
+        listaUsuarios.add(Usuario(3,"Carlos","carlos@gmail.com"))
+        listaUsuarios.add(Usuario(4,"Danny","danny@gmail.com"))
+        listaConteudos.add(ConteudoEducacional(1,"Apresentação", 10,
+            "Apresentação do curso de Kotlin","João",Nivel.BASICO))
+        listaConteudos.add(ConteudoEducacional(2,"Variáveis", 40,
+            "Tipos de Variáveis em Kotlin","João",Nivel.BASICO))
+        listaConteudos.add(ConteudoEducacional(3,"Funções", 60,
+            "Apresentação de funções em Kotlin","João",Nivel.BASICO))
+        listaConteudos.add(ConteudoEducacional(4,"Classes", 60,
+            "Apresentação das classes de Kotlin","João",Nivel.BASICO))
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        val formacaoKotlin:Formacao = Formacao(1,"Formação Kotlin", Nivel.BASICO, listaConteudos,listaUsuarios)
+        formacaoKotlin.adicionarConteudo(ConteudoEducacional(5,"Exceções",60,
+                                "Tratamento de Exceções em Kotlin","João",Nivel.INTERMEDIARIO))
+        formacaoKotlin.removerConteudo(listaConteudos[1])
+        formacaoKotlin.matricular(Usuario(5,"Elias","elias@gmail.com"))
+        formacaoKotlin.removerMatricula(listaUsuarios[3])
+        formacaoKotlin.exibirDetalhes()
     }
-}
-
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
